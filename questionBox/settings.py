a8c193fed093c8524d7579430fa6c3b5fb79ded9
bennykoval <bethanykoval@gmail.com>
2019-07-17 14:35:50 -0400

    Add direction to templates in settings

--- questionBox/settings.py
+++ questionBox/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'questionBox.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': 'core/templates/',
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
