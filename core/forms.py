0ec2019efdaf5f914dedd91f05cf504f9816e3cf
gdickson52 <gdickson52@gmail.com>
2019-07-17 19:12:25 -0400

    views and forms

--- core/forms.py
+++ core/forms.py
@@ -1,3 +1,19 @@
 from django import forms
 
 from core.models import Question, Answer
+
+class QuestionForm(forms.ModelForm):
+    
+    class Meta:
+        model = Question
+        fields = ('question',)
+
+class AnswerForm(forms.ModelForm):
+
+    class Meta:
+        model = Answer
+        fields = (
+            'prompt',
+            'category',
+        )
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from core.models import Question
+from core.forms import QuestionForm
+import django.contrib.auth.decorators 
+
 
 # Create your views here.
+
+def index(request):
+    questions = Question.objects.all()
+
+    context = {
+        'questions': question
+    }
+    
+    return render(request, 'index.html', context)
+
+
+def question(request):
+    question = Question.objects.all()
+
+    context = {
+        'question': question
+    }
+
+    return render(request, 'core/question', context)
+
+
+
