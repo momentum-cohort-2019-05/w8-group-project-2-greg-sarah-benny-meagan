6c1d6392b7e4f0f38e62cc4a994c6f65f5932129
bennykoval <bethanykoval@gmail.com>
2019-07-17 19:39:07 -0400

    Add bee facts

--- core/forms.py
+++ core/forms.py
@@ -1,3 +1,9 @@
 from django import forms
 
 from core.models import Question, Answer
+
+class QuestionForm(forms.ModelForm):
+
+    class Meta:
+        model = Question
+        fields = ('title', 'body',)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from core.models import Question, Answer
+
 # Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+
+    question = Question.objects.all()
+        
+    context = {
+        'question': question,
+    }
+    return render(request, 'index.html', context=context)
+
+    
