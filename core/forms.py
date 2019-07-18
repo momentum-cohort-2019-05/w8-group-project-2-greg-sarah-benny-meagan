8e8fe2847f7f31303f189aaf4db8e2018e4e8d9d
bennykoval <bethanykoval@gmail.com>
2019-07-17 20:52:36 -0400

    Question list view, detail view, and by category done

--- core/forms.py
+++ core/forms.py
@@ -3,7 +3,10 @@ from django import forms
 from core.models import Question, Answer
 
 class QuestionForm(forms.ModelForm):
+    model = Question
+    fields = ('title', 'body', 'categories')
 
-    class Meta:
-        model = Question
-        fields = ('title', 'body',)
+
+class AnswerForm(forms.ModelForm):
+    model = Answer
+    fields = ('title', 'categories')
--- core/views.py
+++ core/views.py
@@ -1,17 +1,37 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.http import HttpResponseRedirect
 from core.models import Question, Answer
-
-# Create your views here.
+from core.forms import QuestionForm, AnswerForm
+from core import views
+# import django.contrib.auth.decorators
+# from django.contrib.auth.decorators import login_required
 
 def index(request):
-    """View function for home page of site."""
+    """View function for home page of site, which includes a list of all questions."""
 
-    question = Question.objects.all()
+    question_list = Question.objects.all()
         
     context = {
-        'question': question,
-    }
-    return render(request, 'index.html', context=context)
+        'question_list': question_list,
+    }    
+    return render(request, 'index.html', context)
 
-    
+def question_detail(request, pk):
+    """Individual question pages, which includes all answers to a given question."""
+
+    question = get_object_or_404(Question, pk=pk)
+
+    return render(request, 'detail.html', {'question':question})
+
+def questions_by_category(request, category_pk):
+    questions_by_category = Question.objects.filter(categories__pk=category_pk)
+    category = get_object_or_404(Category, pk=category_pk)
+
+    context = {
+        'questions_by_category': questions_by_category,
+        'category': category,
+    }
+
+    return render(request, 'questions_by_category.html', context)
+
